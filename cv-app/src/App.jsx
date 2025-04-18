16eeb0c7820181300ef2f0a59518d10500d91279
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-18 14:06:51 +0000

    basic live personal info form

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -2,20 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState('John Smith');
+  const [jobTitle, setJobTitle] = useState('Full Stack Developer');
+  const [email, setEmail] = useState('john.smith@example.com');
+  const [phoneNumber, setPhoneNumber] = useState('+1 (555) 123-4567');
+  const [linkedIn, setLinkedIn] = useState('https://linkedin.com/in/johnsmith');
+  const [github, setGithub] = useState('https://github.com/johnsmith')
 
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
   return (
     <>
-      <p>
-        <label>
-          name: {' '}
-          <input value={name} onChange = {handleNameChange} />
-        </label>
-      </p>
-      <h1>{name}</h1>
+      <div className="inputDetails">
+        <div className="personalDetails">
+          <label>
+            <p className='formLabel'>Full Name</p> 
+            <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+          </label>
+          <label>
+            <p className='formLabel'>Job Title</p> 
+            <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} />
+          </label>
+          <label>
+            <p className='formLabel'>Email</p> 
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            <p className='formLabel'>Phone Number</p> 
+            <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          </label>
+          <label>
+            <p className='formLabel'>LinkedIn Profile</p> 
+            <input type="text" value={linkedIn} onChange={(e) => setLinkedIn(e.target.value)} />
+          </label>
+          <label>
+            <p className='formLabel'>Github Profile</p> 
+            <input type="text" value={github} onChange={(e) => setGithub(e.target.value)} />
+          </label>
+        </div>
+      </div>
+
+      <div className="cvPreview">
+        <div className="personalDetailsPreview">
+          <h1>{fullName}</h1>
+          <h3>{jobTitle}</h3>
+          <div className="reachMeInfo">
+            <p><b>Phone: </b> {phoneNumber} </p>
+            <p><b>Email: </b> {email} </p>
+            <p><b>Github: </b> {github} </p>
+            <p><b>LinkedIn: </b> {linkedIn} </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
