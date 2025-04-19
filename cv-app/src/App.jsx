07bf97dcdbf82393ce7f123e847f21f7fec0870e
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-19 06:05:38 +0000

    basic experience section

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -20,6 +20,12 @@ export default function App() {
   const [skillCategory, setSkillCategory] = useState('');
   const [skills, setSkills] = useState('');
 
+  const [jobExperienceTitle, setJobExperienceTitle] = useState('');
+  const [company, setCompany] = useState('');
+  const [startJob, setStartJob] = useState('');
+  const [endJob, setEndJob] = useState('');
+  const [companyLocation, setCompanyLocation] = useState('');
+  const [responsibility, setResponsibility] = useState('');
   return (
     <>
       <div className="inputDetails">
@@ -86,6 +92,29 @@ export default function App() {
           <p><label htmlFor="">Skills</label></p>
           <input type="text" value={skills} onChange={(e) => setSkills(e.target.value)} />
         </div>
+
+        <div className="experienceDetails">
+          <p className="titleOfDetails">Work Experience</p>
+          <h2>{jobExperienceTitle}</h2>
+
+          <p><label htmlFor="">Job Title</label></p>
+          <input type="text" value={jobExperienceTitle} onChange={(e) => setJobExperienceTitle(e.target.value)} />
+
+          <p><label htmlFor="">Company</label></p>
+          <input type="text" value={company} onChange={(e) => setCompany(e.target.value)}/>
+
+          <p><label htmlFor="">Start Date</label></p>
+          <input type="text" value={startJob} onChange={(e) => setStartJob(e.target.value)} />
+
+          <p><label htmlFor="">End Date</label></p>
+          <input type="text" value={endJob} onChange={(e) => setEndJob(e.target.value)}/>
+
+          <p><label htmlFor="">Location</label></p>
+          <input type="text" value={companyLocation} onChange={(e) => setCompanyLocation(e.target.value)}/>
+
+          <p><label htmlFor="">Responsibilites</label></p>
+          <input type="text" value={responsibility} onChange={(e) => setResponsibility(e.target.value)}/>
+        </div>
       </div>
 
       <div className="cvPreview">
@@ -105,6 +134,17 @@ export default function App() {
             <p>{summary}</p>
         </div>
 
+        <div className="workExperienceInfo">
+          <h3>{jobExperienceTitle}</h3>
+          <div className="actualDetails">
+            <p><b>{company} - </b>{companyLocation}</p>
+            <p>{startJob} - {endJob}</p>
+            <ul>
+              <li>{responsibility}</li>
+            </ul>
+          </div>
+        </div>
+        
         <div className="educationInfo">
           <p className="sectionTitle">Education</p>
 
@@ -125,6 +165,7 @@ export default function App() {
             <span>{skills}</span>
           </div>
         </div>
+
       </div>
     </>
   );
