a4ef1ca512e33f74a2c01d0a08b159e3686d175e
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-20 11:16:44 +0000

    create detailed education card and closed card for education component

--- cv-app/src/components/Education.jsx
+++ cv-app/src/components/Education.jsx
@@ -2,13 +2,110 @@ import { useState } from "react";
 import React from "react";
 import "../App.css";
 
+function DetailedCard({
+  id,
+  degree,
+  school,
+  startDate,
+  endDate,
+  location,
+  handleDelete,
+  handleSave,
+  handleChange,
+}) {
+  return (
+    <>
+      <h2>{degree}</h2>
+      <p>
+        <label htmlFor="">Degree</label>
+      </p>
+      <input name="degree" type="text" value={degree} onChange={handleChange} />
+
+      <p>
+        <label htmlFor="">School or University</label>
+      </p>
+      <input name="school" type="text" value={school} onChange={handleChange} />
+
+      <p>
+        <label htmlFor="">Start Year</label>
+      </p>
+      <input
+        name="startDate"
+        type="text"
+        value={startDate}
+        onChange={handleChange}
+      />
+
+      <p>
+        <label htmlFor="">End Year</label>
+      </p>
+      <input
+        name="endDate"
+        type="text"
+        value={endDate}
+        onChange={handleChange}
+      />
+      <p>
+        <label htmlFor="">Location</label>
+      </p>
+      <input
+        name="location"
+        type="text"
+        value={location}
+        onChange={handleChange}
+      />
+
+      <div>
+        <button onClick={() => handleDelete(id)}>Delete</button>
+        <button onClick={handleSave}>Save</button>
+        <button>Cancel</button>
+      </div>
+    </>
+  );
+}
+function ClosedCard({
+  education,
+  handleEdit
+}) {
+  return (
+    <>
+      <h2>{education.degree}</h2>
+      <div>
+        <button onClick={handleEdit}>Edit</button>
+      </div>
+    </>
+  );
+}
+function Card({
+  education,
+  isOpen,
+  handleChange,
+  handleDelete,
+  handleEdit,
+  handleSave,
+}) {
+  return (
+    <>
+      {isOpen ? (
+        <DetailedCard
+          education={education}
+          handleDelete={handleDelete}
+          handleSave={handleSave}
+          handleChange={handleChange}
+        />
+      ) : (
+        <ClosedCard education={education} handleEdit={handleEdit} />
+      )}
+    </>
+  );
+}
 export default function Education() {
   const [degree, setDegree] = useState("");
   const [school, setSchool] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [location, setLocation] = useState("");
-  const [educationArray, setEducationArray] = useState("");
+  const [educationArray, setEducationArray] = useState([]);
   // const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
 
@@ -34,7 +131,7 @@ export default function Education() {
         break;
     }
   }
-  function handleSave(id) {
+  function handleSave() {
     if (
       degree.trim() !== "" &&
       school.trim() !== "" &&
@@ -46,7 +143,7 @@ export default function Education() {
         const updatedEducationArray = educationArray.map((education) => {
           if (education.id === editId) {
             return {
-              id: id, //because I want the same id property here
+              ...education, //cuz I want the same id
               degree: degree,
               school: school,
               startDate: startDate,
@@ -102,60 +199,16 @@ export default function Education() {
     <>
       <div className="educationDetails">
         <p className="titleOfDetails">Education</p>
-        <p>
-          <label htmlFor="">Degree</label>
-        </p>
-        <input
-          name="degree"
-          type="text"
-          value={degree}
-          onChange={handleChange}
-        />
-
-        <p>
-          <label htmlFor="">School or University</label>
-        </p>
-        <input
-          name="school"
-          type="text"
-          value={school}
-          onChange={handleChange}
-        />
-
-        <p>
-          <label htmlFor="">Start Year</label>
-        </p>
-        <input
-          name="startDate"
-          type="text"
-          value={startDate}
-          onChange={handleChange}
-        />
-
-        <p>
-          <label htmlFor="">End Year</label>
-        </p>
-        <input
-          name="endDate"
-          type="text"
-          value={endDate}
-          onChange={handleChange}
-        />
-        <p>
-          <label htmlFor="">Location</label>
-        </p>
-        <input
-          name="location"
-          type="text"
-          value={location}
-          onChange={handleChange}
-        />
-
-        <div>
-          <button onClick={() => handleDelete()}>Delete</button>
-          <button>Save</button>
-          <button>Cancel</button>
-        </div>
+        {educationArray.map((education) => {
+          <Card
+            education={education}
+            isOpen={editId === education.id ? true : false}
+            handleChange={handleChange}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            handleSave={handleSave}
+          />;
+        })}
       </div>
 
       <div className="educationInfo">
